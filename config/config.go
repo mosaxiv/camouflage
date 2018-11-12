3d752f6f52f962026c82d64d6d551d910a2829fa
mosaxiv <mosaxiv@gmail.com>
2018-11-13 03:08:20 +0900

    init

--- config/config.go
+++ config/config.go
@@ -1,25 +1,87 @@
 package config
 
 import (
+	"log"
 	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type Config struct {
-	Port      string
-	SharedKey string
+	Port              string
+	SharedKey         string
+	HeaderVia         string
+	LengthLimit       int64
+	LogingEnabled     string
+	MaxRedirects      int
+	SocketTimeout     time.Duration
+	TimingAllowOrigin string
+	HostName          string
+	KeepAlive         bool
 }
 
 func NewConfig() Config {
 	return Config{
-		Port:      get("PORT", "8081"),
-		SharedKey: get("POCAMO_KEYRT", "0x24FEEDFACEDEADBEEFCAFE"),
+		Port:              getEnvStr("PORT", "8081"),
+		SharedKey:         getEnvStr("CAMO_KEY", "0x24FEEDFACEDEADBEEFCAFE"),
+		HeaderVia:         getEnvStr("CAMO_HEADER_VIA", "Camo Asset Proxy"),
+		LengthLimit:       getEnvInt64("CAMO_LENGTH_LIMIT", 5242880),
+		LogingEnabled:     getEnvStr("CAMO_LOGGING_ENABLED", "disabled"),
+		MaxRedirects:      getEnvInt("CAMO_MAX_REDIRECTS", 4),
+		SocketTimeout:     time.Duration(getEnvInt("CAMO_SOCKET_TIMEOUT", 10)) * time.Second,
+		TimingAllowOrigin: getEnvStr("CAMO_TIMING_ALLOW_ORIGIN", ""),
+		HostName:          getEnvStr("CAMO_HOSTNAME", "unknown"),
+		KeepAlive:         getEnvBool("CAMO_KEEP_ALIVE", false),
 	}
 }
 
-func get(key, def string) string {
+func getEnvStr(key, def string) string {
 	v := os.Getenv(key)
 	if v == "" {
 		return def
 	}
 	return v
+}
+
+func getEnvInt(key string, def int) int {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		log.Fatalf("%s : %s", key, err.Error())
+	}
+
+	return i
+}
+
+func getEnvInt64(key string, def int64) int64 {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+	i, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		log.Fatalf("%s : %s", key, err.Error())
+	}
+
+	return i
+}
+
+func getEnvBool(key string, def bool) bool {
+	v := os.Getenv(key)
+	if v == "" {
+		return def
+	}
+
+	switch strings.ToLower(v) {
+	case "true", "1":
+		return true
+	case "false", "0":
+		return false
+	default:
+		return false
+	}
 }
--- main.go
+++ main.go
@@ -8,9 +8,11 @@ import (
 	"net/http"
 	"os"
 
+	"errors"
 	"github.com/go-chi/chi"
 	"github.com/mosaxiv/camouflage/config"
 	"github.com/mosaxiv/camouflage/hash"
+	"strconv"
 )
 
 type Param struct {
@@ -19,10 +21,11 @@ type Param struct {
 }
 
 var hmac hash.HMAC
+var conf config.Config
 
 func main() {
-	c := config.NewConfig()
-	hmac = hash.NewHMAC(c.SharedKey)
+	conf = config.NewConfig()
+	hmac = hash.NewHMAC(conf.SharedKey)
 
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
@@ -37,9 +40,9 @@ func main() {
 	r.Get("/{digest}", query)
 	r.Get("/{digest}/{url}", param)
 
-	fmt.Printf("SSL-Proxy running on %s pid: %d\n", c.Port, os.Getpid())
+	fmt.Printf("SSL-Proxy running on %s pid: %d\n", conf.Port, os.Getpid())
 
-	if err := http.ListenAndServe(":"+c.Port, r); err != nil {
+	if err := http.ListenAndServe(":"+conf.Port, r); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
@@ -97,16 +100,72 @@ func (p *Param) proxy(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 
-	client := new(http.Client)
+	if h := r.Header.Get("Accept"); h == "" {
+		req.Header.Set("Accept", "image/*")
+	} else {
+		req.Header.Set("Accept", h)
+	}
+	if h := r.Header.Get("Accept-Encoding"); h == "" {
+		req.Header.Set("Accept-Encoding", "")
+	} else {
+		req.Header.Set("Accept-Encoding", h)
+	}
+	req.Header.Set("Via", conf.HeaderVia)
+	req.Header.Set("User-Agent", conf.HeaderVia)
+	req.Header.Set("X-Frame-Options", "deny")
+	req.Header.Set("X-XSS-Protection", "1; mode=block")
+	req.Header.Set("X-Content-Type-Options", "nosniff")
+	req.Header.Set("Content-Security-Policy", "default-src 'none'; img-src data:; style-src 'unsafe-inline'")
+
+	client := &http.Client{
+		Timeout: conf.SocketTimeout,
+		Transport: &http.Transport{
+			DisableKeepAlives: !conf.KeepAlive,
+		},
+	}
+
 	res, err := client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
 
+	if res.ContentLength > conf.LengthLimit {
+		return errors.New("Content-Length exceeded")
+	}
+
 	_, err = io.Copy(w, res.Body)
 	if err != nil {
 		return err
+	}
+
+	if h := res.Header.Get("Cache-Control"); h == "" {
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+	} else {
+		w.Header().Set("Cache-Control", h)
+	}
+
+	w.Header().Set("Content-Type", res.Header.Get("Content-Type"))
+	w.Header().Set("Content-Length", strconv.FormatInt(res.ContentLength, 10))
+	w.Header().Set("Transfer-Encoding", res.Header.Get("Transfer-Encoding"))
+	w.Header().Set("Content-Encoding", res.Header.Get("Content-Encoding"))
+	w.Header().Set("Camo-Host", conf.HostName)
+	w.Header().Set("X-Frame-Options", "deny")
+	w.Header().Set("X-XSS-Protection", "1; mode=block")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Content-Security-Policy", "default-src 'none'; img-src data:; style-src 'unsafe-inline'")
+	w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+	if h := res.Header.Get("etag"); h != "" {
+		w.Header().Set("etag", h)
+	}
+	if h := res.Header.Get("expires"); h != "" {
+		w.Header().Set("expires", h)
+	}
+	if h := res.Header.Get("last-modified"); h != "" {
+		w.Header().Set("last-modified", h)
+	}
+	if conf.TimingAllowOrigin != "" {
+		w.Header().Set("Timing-Allow-Origin", conf.TimingAllowOrigin)
 	}
 
 	return nil
